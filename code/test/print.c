1bc36c9f4db03b085507e183e57f901824e8cbdf
Lai-YT <381xvmvbib@gmail.com>
2022-03-19 13:07:36 +0800

    test: more robust on test case

--- code/test/print.c
+++ code/test/print.c
@@ -3,7 +3,9 @@
 
 int main (void) {
 	// Print("This is a message: ");
-	PrintInt(666);
+	PrintInt(123);	/* positive */
+	PrintInt(-123);	/* negative */
+	PrintInt(0);		/* zero */
 	// Print("\n");
 	Halt();
 	/* not reached */
